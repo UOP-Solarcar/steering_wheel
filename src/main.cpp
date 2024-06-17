e4e89f21fb12d5f9e26043b836a8410acaa82fd3
MiguelX413 <lbpgaming27@gmail.com>
2024-06-16 18:12:32 -0700

    Write basic SPI code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <Arduino.h>
+#include <SPI.h>
+
+// Basically defaults besides 12Mhz
+SPISettings settings(12000000, MSBFIRST, SPI_MODE0);
+
+const uint32_t CS = 10;
+
 void setup() {
-// write your initialization code here
+  Serial.begin(MONITOR_SPEED);
+  while (!Serial)
+    ;
+  Serial.print("Booted");
+  pinMode(CS, OUTPUT);
+  digitalWrite(CS, HIGH);
+  SPI.begin();
+  SPI.setClockDivider(0);
 }
 
 void loop() {
-// write your code here
+  SPI.beginTransaction(settings);
+  digitalWrite(CS, LOW);
+  delay(25);
+  auto byte = SPI.transfer(0x00);
+  Serial.println(byte, HEX);
+  digitalWrite(CS, HIGH);
+  SPI.endTransaction();
+  delay(25);
 }
